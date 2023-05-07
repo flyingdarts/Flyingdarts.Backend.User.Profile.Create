b3ba1ac9f88610a524f81c953e835b155048e7d8
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-07 13:56:38 +0200

    Dont ignore email

--- CQRS/CreateUserProfileCommand.cs
+++ CQRS/CreateUserProfileCommand.cs
@@ -1,12 +1,10 @@
-﻿using System.Text.Json.Serialization;
-using Amazon.Lambda.APIGatewayEvents;
+﻿using Amazon.Lambda.APIGatewayEvents;
 using MediatR;
 
 public class CreateUserProfileCommand : IRequest<APIGatewayProxyResponse>
 {
     public string CognitoUserId { get; set; }
     public string UserName { get; set; }
-    [JsonIgnore]
     public string Email { get; set; }
     public string Country { get; set; }
 }
